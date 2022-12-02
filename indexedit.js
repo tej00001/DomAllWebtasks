63513a06c493085de0a8093d41ebe181c97b69bf
tej00001 <tejachintu207@gmail.com>
2022-12-02 09:27:45 +0530

    adding getElementsByTagName

---
+++ indexedit.js
@@ -0,0 +1,34 @@
+//console.dir(document)
+
+//console.log(document.visibilityState)
+
+//console.log(document.hidden)
+
+/*var items= document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+
+
+
+items[2].style.backgroundColor = "green";
+
+for(let i=0;i<items.length;i++)
+{
+items[i].style.fontWeight = "bold";
+
+}*/
+
+
+var li= document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+
+
+
+li[2].style.backgroundColor = "green";
+
+for(let i=0;i<li.length;i++)
+{
+li[i].style.fontWeight = "bold";
+
+}
